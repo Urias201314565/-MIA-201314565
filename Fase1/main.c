4792d686b752dc93ea31b8bcdd10ae7e462e268b
Urias201314565 <d14urias@gmail.com>
2016-08-02 19:53:15 -0600

    Prueba commit

--- Fase1/main.c
+++ Fase1/main.c
@@ -8,8 +8,13 @@ int main()
     printf("Hello world!\n");
     }
 
+    for(i=0;i<10;i++){
+    printf("Hello world!\n");
+    }
 
-
+    for(i=0;i<10;i++){
+    printf("Hello world!\n");
+    }
 
 
     return 0;
